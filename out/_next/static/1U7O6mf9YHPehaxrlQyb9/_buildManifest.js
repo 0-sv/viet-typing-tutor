e1ccc8ffb3bfd43ec7a1c458bb2c19b88e173df0
A.J.S. Verbeek <ajsverbeek@gmail.com>
2023-04-25 10:15:56 +0200

    "Deploy"

---
+++ out/_next/static/1U7O6mf9YHPehaxrlQyb9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ecdf16dadf5ee87f.css","static/chunks/pages/index-b3aa3323bc94cc99.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
