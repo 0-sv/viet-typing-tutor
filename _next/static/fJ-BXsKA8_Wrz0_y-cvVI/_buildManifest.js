710f181270ddf3edc8cce9f5f0139bd28e900df8
A.J.S. Verbeek <ajsverbeek@gmail.com>
2023-04-26 14:34:57 +0200

    "Deploy-to-gh-pages"

--- _next/static/fJ-BXsKA8_Wrz0_y-cvVI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/900-de9d81c40d16e77c.js","static/css/ecdf16dadf5ee87f.css","static/chunks/pages/index-519ccae7bb18e3fa.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
